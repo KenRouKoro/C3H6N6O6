6177ad71e9e009c0a7ac5d9c8b12c22c684cb953
KenRouKoro <koro@korostudio.cn>
2022-07-04 05:40:33 +0800

    1.0.0-alpha1 README Update

--- src/main/java/cn/korostudio/c3h6n6o6/C3H6N6O6.java
+++ src/main/java/cn/korostudio/c3h6n6o6/C3H6N6O6.java
@@ -6,14 +6,16 @@ import cn.hutool.setting.Setting;
 import cn.korostudio.c3h6n6o6.thread.CalculationController;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.api.ModInitializer;
 
 @Slf4j
-public class C3H6N6O6 implements ModInitializer {
+public class C3H6N6O6 implements DedicatedServerModInitializer {
     public static final Setting Setting = new Setting(FileUtil.touch(System.getProperty("user.dir") + "/config/C3H6N6O6.setting"), CharsetUtil.CHARSET_UTF_8, true);
     public static Thread ServerThread;
+
     @Override
-    public void onInitialize() {
+    public void onInitializeServer() {
         log.info("C3H6N6O6 Is Loading!");
         log.info(Thread.currentThread().getName());
         CalculationController.Init();
